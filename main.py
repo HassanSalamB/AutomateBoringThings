60aff3a9f2ab5567ed675f2c81af605c03398757
HassanSB <73503151+HassanSalamB@users.noreply.github.com>
2021-09-16 15:04:20 +0200

    adding files initially

---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+import time
+
+import unittest
+
+
+class GoogleSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            "C://Users//madmin//Documents//XDocs//Data_Science//Github_repos//AutomateBoringThings//chromedriver.exe")
+
+    def test_googlesearch(self):
+        self.driver.get('https://www.google.com/')
+        time.sleep(2)
+
+        q = self.driver.find_element(By.NAME, 'q')
+
+        q.send_keys('webdriver')
+
+        q.submit()
+        time.sleep(2)
+
+        print(self.driver.current_url)
+        time.sleep(4)
+
+    def exit(self):
+        self.driver.close()
