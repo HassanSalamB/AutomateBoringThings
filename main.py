3b9cb7b9db3fcea79a2955fce63aef66381835c8
HassanSB <73503151+HassanSalamB@users.noreply.github.com>
2021-09-20 15:17:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ import unittest
 
 class GoogleSearch(unittest.TestCase):
     def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
         self.driver = webdriver.Chrome(
-            "/var/jenkins_home/workspace/PythonGoogleSearch/chromedriver")
+            "/var/jenkins_home/workspace/PythonGoogleSearch/chromedriver", chrome_options=chrome_options)
 
     def test_googlesearch(self):
         self.driver.get('https://www.google.com/')
